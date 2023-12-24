5f9b909b226f39310b3cb2e4b48b7ba6dc47fcc9
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-23 19:58:22 -0600

    Add authentication page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,8 @@ const dateOptions: { date: number, month: number, year: number, description: str
 
 export default function Home() {
   const [ids, setIds] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
   
   async function loadIds() {
     const res: Response = await fetch('/api/schedule');
@@ -102,50 +104,75 @@ export default function Home() {
     await loadIds();
   }
 
+  function handleAuth(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    if ((e.target as HTMLFormElement).authPassword.value === process.env.NEXT_PUBLIC_WEBSITE_PASSWORD) {
+      setAuthenticated(true);
+    }
+  }
+
+
   return (
     <>
-      <div className="m-auto w-80">
-        <form onSubmit={handleSubmit} className="flex flex-col">
-          <label htmlFor="username">Username</label>
-          <input type="text" name="username" placeholder="Username" className="border border-black" />
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" placeholder="Password" className="border border-black" />
-          <label htmlFor="date">Date</label>
-          <select name="date" className="border border-black">
-            {dateOptions.map((dateOption, idx) => (
-              <option key={idx} value={idx}>{dateOption.description}</option>
-            ))}
-          </select>
-          <label htmlFor="startTime">Start Time</label>
-          <select name="startTime" defaultValue={23} className="border border-black">
-            {timeOptions.map((timeOption, idx) => (
-              <option key={idx} value={idx}>{timeOption}</option>
-            ))}
-          </select>
-          <label htmlFor="endTime">End Time</label>
-          <select name="endTime" defaultValue={27} className="border border-black">
-            {timeOptions.map((timeOption, idx) => (
-              <option key={idx} value={idx}>{timeOption}</option>
-            ))}
-          </select>
-          <label htmlFor='courtOrder'>Court Order</label>
-          <input type='text' name='courtOrder' defaultValue={'6,4,3,1,5,2'} placeholder='Comma-separated order (e.g. 1,2,3)' className='border border-black'/>
-          <button type="submit" className="border border-black bg-green-300">Submit</button>
-        </form>
-      </div>
-      <div>
-        <div>
-          Current jobs
+      {!authenticated ? (
+        <>
+          <div className='h-screen w-screen flex justify-center items-center text-lg'>
+            <form onSubmit={handleAuth} className='flex flex-col justify-center items-start'>
+              <label htmlFor="authPassword" className='font-bold mb-1'>Enter password:</label>
+              <input type="password" name="authPassword" placeholder="Password" className='border border-black shadow-md rounded-md px-3 py-2 mb-3' />
+              <button type="submit" className='bg-green-600 shadow-md rounded-md px-3 py-2 text-white transition-transform hover:-translate-y-1'>Submit</button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <div className='w-screen flex flex-wrap justify-center items-center'>
+          <div>
+            <form onSubmit={handleSubmit} className="flex flex-col">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" placeholder="Username" className="border border-black" />
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" placeholder="Password" className="border border-black" />
+              <label htmlFor="date">Date</label>
+              <select name="date" className="border border-black">
+                {dateOptions.map((dateOption, idx) => (
+                  <option key={idx} value={idx}>{dateOption.description}</option>
+                ))}
+              </select>
+              <label htmlFor="startTime">Start Time</label>
+              <select name="startTime" defaultValue={23} className="border border-black">
+                {timeOptions.map((timeOption, idx) => (
+                  <option key={idx} value={idx}>{timeOption}</option>
+                ))}
+              </select>
+              <label htmlFor="endTime">End Time</label>
+              <select name="endTime" defaultValue={27} className="border border-black">
+                {timeOptions.map((timeOption, idx) => (
+                  <option key={idx} value={idx}>{timeOption}</option>
+                ))}
+              </select>
+              <label htmlFor='courtOrder'>Court Order</label>
+              <input type='text' name='courtOrder' defaultValue={'6,4,3,1,5,2'} placeholder='Comma-separated order (e.g. 1,2,3)' className='border border-black'/>
+              <button type="submit" className="border border-black bg-green-300">Submit</button>
+            </form>
+          </div>
+
+          <hr className='block md:hidden h-px border border-gray-400 my-3 w-5/6'></hr>
+
+          <div>
+            <div>
+              Current jobs
+            </div>
+            <div>
+              {ids.map((id, idx) => (
+                <div key={idx}>
+                  {idToDescription(id)}
+                  <button type="button" onClick={deleteJob} name={id} className='border border-black bg-red-200'>Cancel</button>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-        <div>
-          {ids.map((id, idx) => (
-            <div key={idx}>
-              {idToDescription(id)}
-              <button type="button" onClick={deleteJob} name={id} className='border border-black bg-red-200'>Cancel</button>
-            </div>                
-          ))}
-        </div>
-      </div>
+      )}
     </>
   )
 }
