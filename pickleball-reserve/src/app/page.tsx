ede8cc77b2d44734510851e9302582feab6ed604
Michael Zhan <michaelzhan2001@gmail.com>
2025-01-17 22:57:45 -0600

    Add basic cookie auth
    TODO: Make auth page create cookie with TTL
    
    TODO: make TTL refresh on page load

--- pickleball-reserve/src/app/page.tsx
+++ pickleball-reserve/src/app/page.tsx
@@ -1,11 +1,33 @@
-"use client";
+import { cookies } from 'next/headers';
+import { redirect } from "next/navigation";
 
-import { useState } from "react";
 
+async function checkAuth(value: string): Promise<boolean> {
+  const res: Response = await fetch(`${process.env.API_URL}/api/auth`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ password: value })
+  });
 
-export default function Home() {
-  const [authenticated, setAuthenticated] = useState<boolean>(false);
-  
+  return res.ok;
+}
+
+export default async function Home() {
+  // check for auth cookie with correct value
+  const cookieStore = await cookies();
+  const authCookie = cookieStore.get('auth');
+
+  if (authCookie === undefined) {
+    redirect(`${process.env.API_URL}/auth`);
+  } else {
+    const isAuth = await checkAuth(authCookie.value);
+    if (!isAuth) {
+      redirect(`${process.env.API_URL}/auth`);
+    }
+  }
+
   return (
     <>
       <div>Hello world!</div>
