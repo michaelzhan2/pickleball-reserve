ca4beeca6ffdc6480612f7d38498ed76a6f40510
Michael Zhan <michaelzhan2001@gmail.com>
2025-01-20 21:48:44 -0600

    Set up basic api infra

--- redo/backend/src/index.ts
+++ redo/backend/src/index.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from 'express';
-import { log } from '@utils/logger';
+import cors from 'cors';
 import dotenv from 'dotenv';
+
+import { log } from '@utils/logger';
+import { helloWorldHandler } from '@handlers/index';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+// cors
+const corsOptions: cors.CorsOptions = {
+  origin: "http://localhost:8080"
+};
+app.use(cors(corsOptions));
+
+app.get('/', helloWorldHandler);
 
 app.listen(port, () => {
   log(`[server]: Server is running at https://localhost:${port}`);
